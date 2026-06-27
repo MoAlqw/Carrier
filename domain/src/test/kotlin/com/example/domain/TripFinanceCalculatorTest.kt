9f057acb9d17697f27afd6d367eae73ba4f95aca
nikita <nikitaguzenko2@gmail.com>
2026-06-27 23:05:05 +0500

    Some fix

---
+++ domain/src/test/kotlin/com/example/domain/TripFinanceCalculatorTest.kt
@@ -0,0 +1,90 @@
+package com.example.domain
+
+import com.example.domain.model.TripFinanceCalculator
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class TripFinanceCalculatorTest {
+
+    @Test
+    fun `grossProfit should return income minus expenses`() {
+        val income = 100_000L
+        val expenses = 50_000L
+
+        val result = TripFinanceCalculator.grossProfit(
+            income = income,
+            expenses = expenses
+        )
+
+        assertThat(result).isEqualTo(50_000L)
+    }
+
+    @Test
+    fun `grossProfit should return a negative value if expenses are more than income`() {
+        val income = 50_000L
+        val expenses = 100_000L
+
+        val result = TripFinanceCalculator.grossProfit(
+            income = income,
+            expenses = expenses
+        )
+
+        assertThat(result).isEqualTo(50_000L - 100_000L)
+    }
+
+    @Test
+    fun `tax should return 12 percent of grossProfit`() {
+        val grossProfit = 50_000L
+
+        val result = TripFinanceCalculator.tax(grossProfit)
+
+        assertThat(result).isEqualTo(6_000L)
+    }
+
+    @Test
+    fun `tax should return 0 if grossProfit is negative`() {
+        val grossProfit = -50_000L
+
+        val result = TripFinanceCalculator.tax(grossProfit)
+
+        assertThat(result).isEqualTo(0L)
+    }
+
+    @Test
+    fun `netProfit should return grossProfit minus tax`() {
+        val grossProfit = 50_000L
+
+        val result = TripFinanceCalculator.netProfit(grossProfit)
+
+        assertThat(result).isEqualTo(44_000L)
+    }
+
+    @Test
+    fun `netProfit should return 0 if grossProfit is negative`() {
+        val grossProfit = -50_000L
+
+        val result = TripFinanceCalculator.netProfit(grossProfit)
+
+        assertThat(result).isEqualTo(0L)
+    }
+
+    @Test
+    fun `profitability should return netProfit divided by income`() {
+        val income = 100_000L
+        val netProfit = 44_000L
+
+        val result = TripFinanceCalculator.profitability(income, netProfit)
+
+        assertThat(result).isEqualTo(44L)
+    }
+
+    @Test
+    fun `profitability should return 0 if income is 0`() {
+        val income = 0L
+        val netProfit = 0L
+
+        val result = TripFinanceCalculator.profitability(income, netProfit)
+
+        assertThat(result).isEqualTo(0L)
+    }
+}
